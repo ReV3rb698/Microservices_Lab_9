02c792ba5ff42edfd4792e56ff24c62007d7ee84
ReV3rb698 <146119522+ReV3rb698@users.noreply.github.com>
2025-04-01 01:45:31 -0700

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -71,7 +71,7 @@ def submit_telemetry_data(body):
     return NoContent, status_code
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api('./openapi.yml', strict_validation=True, validate_responses=True)
+app.add_api('./openapi.yml', base_path="/receiver", strict_validation=True, validate_responses=True)
 
 if __name__ == '__main__':
     app.run(port=8080, host="0.0.0.0")
