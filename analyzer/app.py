b3a634c3f6af515876a0d7c28095e8b54c281958
ReV3rb698 <xetron34@gmail.com>
2025-04-08 01:29:24 -0700

    fxied config load

--- analyzer/app.py
+++ analyzer/app.py
@@ -14,7 +14,9 @@ from starlette.middleware.cors import CORSMiddleware
 os.environ["LOG_FILENAME"] = "/app/logs/analyzer.log"
 
 # Load logging configuration
-with open("/app/config/log_config.yml", "r") as f:
+with open("/app/analyzer_config.yml", "r") as f:
+    app_config = yaml.safe_load(f)
+with open("/app/analyzer_config.yml", "r") as f:
     log_config = yaml.safe_load(f)
 logging.config.dictConfig(log_config)
 logger = logging.getLogger("basicLogger")
